cf5e9919fe719cd01f8be7763bcdc7230617c261
Lliam <lliam@plusnarrative.com>
2023-09-02 15:35:28 +0200

    init: first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# beth-stack
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.tsx
+bun run --watch index.tsx
+```
+
+This project was created using `bun init` in bun v0.8.1. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.tsx
@@ -0,0 +1,71 @@
+import { Elysia } from 'elysia';
+import { html } from '@elysiajs/html';
+import * as elements from 'typed-html';
+
+// State
+
+let count = 0;
+
+// App
+
+const app = new Elysia();
+
+// Routes
+
+app.use(html())
+    .get('/', ({ html }) =>
+        html(
+            <BaseHtml>
+                <body
+                    class="flex w-full h-screen justify-center items-center"
+                    hx-get="/button"
+                    hx-trigger="load"
+                    hx-swap="innerHTML"
+                />
+            </BaseHtml>
+        )
+    )
+    .get('/button', () => <Button />)
+    .post('/click', () => {
+        count++;
+        return <Button />;
+    })
+    .listen(3000);
+
+// Server listening
+
+console.log(
+    `Server running at http://${app?.server?.hostname}:${app?.server?.port}/`
+);
+
+// Base HTML
+
+const BaseHtml = ({ children }: elements.Children) => `
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Elysia</title>
+        <script src="https://unpkg.com/htmx.org@1.9.5"></script>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+    ${children}
+</html>
+`;
+
+// Components
+
+function Button() {
+    return (
+        <button
+            class="bg-slate-900 text-white px-6 py-2 rounded-md"
+            hx-post="/click"
+            hx-swap="outerHTML"
+        >
+            Click Me ({count})
+        </button>
+    );
+}
+
+// Run
+// bun run --watch index.tsx
